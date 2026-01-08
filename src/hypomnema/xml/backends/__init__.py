a9ca1fd286478f6f7f7d7ed883bcda6b1cef472f
Enzo Agosta <agosta.enzowork@gmail.com>
2026-01-08 16:31:16 +0100

    bump python version to py3.14, update packages to latest and run ruff/ty everywhere

--- src/hypomnema/xml/backends/__init__.py
+++ src/hypomnema/xml/backends/__init__.py
@@ -1,13 +1,13 @@
 from warnings import warn
 
-from hypomnema.xml.backends.base import XmlBackend
-from hypomnema.xml.backends.standard import StandardBackend
+from .base import XmlBackend
+from .standard import StandardBackend
+
+__all__ = ["XmlBackend", "StandardBackend"]
 
 try:
   from hypomnema.xml.backends.lxml import LxmlBackend  # noqa: F401
 
-except ImportError as e:
-  warn(f"lxml not installed, Only StandardBackend will be available. Error: {e}")
-  LxmlBackend = None  # type:ignore
-
-__all__ = ["XmlBackend", "StandardBackend", "LxmlBackend"]
+  __all__.append("LxmlBackend")
+except ImportError:
+  warn("lxml not installed, Only StandardBackend will be available")
