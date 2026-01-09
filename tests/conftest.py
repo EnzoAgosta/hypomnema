c72a58145f3207ac8e68c386167d6b798b4557b4
Enzo Agosta <agosta.enzowork@gmail.com>
2026-01-09 15:22:36 +0100

    Cleanup tests for sweeping change

--- tests/conftest.py
+++ tests/conftest.py
@@ -17,11 +17,10 @@ TypeOfBackend = StandardBackend | LxmlBackend | StrictBackend
 @pytest.fixture(
   params=["StandardBackend", "LxmlBackend", "StrictBackend"], ids=["Standard", "Lxml", "Strict"]
 )
-def backend(request):
+def backend(request, test_logger):
   """
   Parametrized fixture that yields a fresh instance of each backend.
   """
-  test_logger = logging.getLogger("test")
   match request.param:
     case "StandardBackend":
       yield StandardBackend(logger=test_logger)
@@ -29,3 +28,11 @@ def backend(request):
       yield LxmlBackend(logger=test_logger)
     case "StrictBackend":
       yield StrictBackend(logger=test_logger)
+
+
+@pytest.fixture(scope="session")
+def test_logger():
+  """Test logger."""
+  test_logger = logging.getLogger("test")
+  test_logger.setLevel(1)
+  return test_logger
